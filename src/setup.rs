b626f5dcc8cf9a4a5352790327fa3d3b5cd0ffa5
Luiz Miguel <contato.luiz404@gmail.com>
2026-05-09 22:42:46 -0300

    Update setup.rs

--- src/setup.rs
+++ src/setup.rs
@@ -2,19 +2,17 @@ use std::fs;
 use std::path::Path;
 
 pub fn run_setup() {
-    println!("Status: Initializing HEML Infrastructure...");
+    println!("\x1b[33m[SETUP] Provisioning System Infrastructure...\x1b[0m");
 
     let dirs = ["C:\\heml", "C:\\heml\\bin", "C:\\heml\\components"];
     for dir in dirs {
         if !Path::new(dir).exists() {
-            fs::create_dir_all(dir).expect("Failed to create directory");
-            println!("Folder created: {}", dir);
+            fs::create_dir_all(dir).ok();
+            println!("\x1b[32m[OK] Created Directory: {}\x1b[0m", dir);
         }
     }
 
-    let welcome_comp = "C:\\heml\\components\\welcome.heml";
-    let welcome_data = "<center><h1>HEML READY</h1></center>";
-    
-    fs::write(welcome_comp, welcome_data).ok();
-    println!("Final Status: Environment is ready.");
+    let welcome = "C:\\heml\\components\\welcome.heml";
+    fs::write(welcome, "<center><h1>HEML KERNEL v0.1.0 ACTIVE</h1></center>").ok();
+    println!("\x1b[32m[READY] Environment fully initialized.\x1b[0m");
 }
